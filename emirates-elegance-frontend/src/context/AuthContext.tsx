f621fc910466f5427a106d73508b5703a403e187
Sushmita67 <biswakarmasushmita5@gmail.com>
2025-02-02 15:26:12 +0545

    deleted files

--- emirates-elegance-frontend/src/context/AuthContext.tsx
+++
@@ -1,7 +0,0 @@
-// import {createContext} from "react";
-//
-// export const AuthContext=createContext();
-//
-// export const AuthProvider=({children})=>{
-//     const  [info, setInfo]
-// }
--- emirates-elegance-frontend/src/pages/BrowseDetails.tsx
+++
@@ -1,9 +0,0 @@
-import { Browse } from "@/components";
-
-export default function BrowseDetails() {
-    return (
-        <div className="md:container container-fluid">
-            <Browse />
-        </div>
-    )
-}
